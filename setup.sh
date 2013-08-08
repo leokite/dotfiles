ca78e675ccdd72c9f21bacebf7a35fe50c813edc
leokite <yuji.kazama.dev@gmail.com>
2013-08-08 22:05:13 +0900

    proxy

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# synbolic link
 DOT_FILES=(.gitconfig .tmux.conf .vimrc .zshrc .zshrc.custom .zshrc.alias .zshrc.osx .zshrc.linux)
-
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
 # install oh-my-zsh
-[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[! -d ~/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # install z
 cd $HOME
@@ -16,4 +16,4 @@ git clone https://github.com/rupa/z.git
 
 # install Neobundle
 mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
